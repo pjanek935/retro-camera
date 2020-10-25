acb46925ec91a8caba9fc43ef40d681383a76e4e
ppj-mobo <ppj@mobostudio.com>
2020-10-25 15:35:44 +0100

    first commit

---
+++ Assets/downres-camera/CameraPostProcessEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraPostProcessEffect : MonoBehaviour
+{
+    [SerializeField] protected Material material = null;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        preRenderImage(source, destination);
+
+        if (material != null)
+        {
+            Graphics.Blit(source, destination, material);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+
+    protected virtual void preRenderImage (RenderTexture source, RenderTexture destination)
+    {
+
+    }
+}
---
+++ Assets/downres-camera/Post Process/ColorInversion.shader
@@ -0,0 +1,31 @@
+﻿Shader "PostProcess/ColorInversion"
+{
+	Properties
+	{
+		_MainTex("Base (RGB)", 2D) = "white" {}
+		_Strength("Distortion Strength", Float) = 0.001
+	}
+
+		SubShader
+		{
+			Pass
+			{
+				CGPROGRAM
+				#pragma vertex vert_img
+				#pragma fragment frag
+				#include "UnityCG.cginc"
+
+				uniform sampler _MainTex;
+				fixed _Strength;
+
+				fixed4 frag(v2f_img i) : COLOR
+				{
+					fixed4 base = tex2D(_MainTex, i.uv);
+
+					return _Strength * (1 - base) + (1 - _Strength) * base;
+				}
+
+				ENDCG
+			}
+		}
+}
